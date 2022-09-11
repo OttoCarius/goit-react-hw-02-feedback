cac497869e6ad43a93761a936c6e72fa37c267dd
OttoCarius <ottocarius1993@gmail.com>
2022-09-11 21:39:07 +0300

    finish 1 task without style

---
+++ src/components/Counter/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul>
+      {options.map(option => {
+        return (
+          <li key={option}>
+            <button onClick={onLeaveFeedback} option={option}>
+              {option}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func,
+};
+
+export default FeedbackOptions;
